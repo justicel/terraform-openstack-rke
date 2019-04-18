9e4fe46177de3d0d67b26865c56bdc7909b59780
Justice London <jlondon@syrussystems.com>
2019-04-17 22:01:43 -0700

    Syntax fix

--- modules/rke/output.tf
+++ modules/rke/output.tf
@@ -2,11 +2,11 @@ output kube_config_cluster {
   description = "Kubeconfig file"
 
   # Workaround: https://github.com/rancher/rke/issues/705
-  value     = "${replace(coalesce("", rke_cluster.cluster_no_bastion.*.kube_config_yaml, rke_cluster.cluster.*.kube_config_yaml), local.api_access_regex, local.api_access)}"
+  value     = "${replace(coalesce("", join("", rke_cluster.cluster_no_bastion.*.kube_config_yaml), join("", rke_cluster.cluster.*.kube_config_yaml)), local.api_access_regex, local.api_access)}"
   sensitive = true
 }
 
 output cluster_yml {
   description = "RKE cluster.yml file"
-  value       = "${coalesce("", rke_cluster.cluster_no_bastion.*.rke_cluster_yaml, rke_cluster.cluster.*.rke_cluster_yaml)}"
+  value       = "${coalesce("", join("", rke_cluster.cluster_no_bastion.*.rke_cluster_yaml), join("", rke_cluster.cluster.*.rke_cluster_yaml))}"
 }
